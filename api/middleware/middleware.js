06cb913c38c135623f4c498434de93451302d447
Arison13 <arisonarias@gmail.com>
2021-12-01 23:29:42 -0500

    working on middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,54 @@
-function logger(req, res, next) {
+const User = require('../users/users-model'); 
+
+ function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timestamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalurl
+  console.log(`at [${timestamp}] method: ${method} to ${url}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+     if (!user) {
+      res.status(404).json({
+        message: "user not found",
+      })
+     } else {
+       req.user = user
+       next()
+     }
+  }
+  catch (err) {
+    res.status(500).json({
+      message: "problem finding user",
+    })
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  const {name} = req.body
+  if (!name){
+    res.status(400).json({
+      message:"missing required name filed"
+    })
+  }else{
+    res.name = name;
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-}
+} 
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+}
